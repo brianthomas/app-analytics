be0154753ce567746ca2d5ea23f17cf86e1163c4
Brian Thomas <brian.a.thomas@nasa.gov>
2018-01-05 20:33:47 -0500

    working schema and loader for process data

--- bin/load_db.py
+++ bin/load_db.py
@@ -5,13 +5,142 @@ import psycopg2
 import pandas as pd
 import logging
 
+logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.DEBUG)
+LOG.setLevel(logging.INFO)
 
-def create_connection (dbname):
+def _create_connection (dbname: str) -> psycopg2.extensions.connection:
+    ''' open connection to local postgresql server instance '''
     return psycopg2.connect(host="localhost",database=dbname, user="postgres", password="")
 
-def parse(file: str) -> pd.DataFrame:
+def _checkmaps(conn: psycopg2.extensions.connection, col: str, tbl: str, file_itms: set):
+    ''' check and remap data '''
+
+    cur = conn.cursor()
+    col = col.replace (" ", "_")
+    col = col.lower()
+
+    LOG.debug(f'''SELECT DISTINCT "{col}" from {tbl};''')
+    cur.execute(f'''SELECT DISTINCT "{col}" from {tbl};''')
+    query = cur.fetchall()
+    existing_items = set([i[0] for i in query])
+
+    for each in file_itms:
+        if each not in existing_items:
+            cur.execute(f'''INSERT INTO {tbl} ("{col}") VALUES ('{each}');''')
+            LOG.debug(f"Inserted new item {col}: {each} into table {tbl}")
+
+    conn.commit()
+
+def _insert_process_files(dbname: str, fname: str):
+
+    # parse out out data into in-memory DataFrames
+    data = _parse(fname)
+
+    LOG.info("Writing {0} to database".format(fname))
+
+    conn = _create_connection(dbname)
+    cur = conn.cursor()
+
+    LOG.info ("Doing mapping checks")
+    mapping_check = {
+            "Center Name": "center_map",\
+            "Device Name": "device_map",\
+            "Process Path": "process_map",\
+#            "Operating System": "os_map",\
+#            "OS Version": "os_version_map",\
+                    }
+
+    for col, tbl in mapping_check.items():
+        _checkmaps(conn, col, tbl, set(data[col].unique()) )
+
+    # remap center to an id
+    cur.execute('select rowid, center_name from center_map;')
+    center_dict = {k:v for v, k in cur.fetchall()}
+
+    # remap device to an id
+    cur.execute('select rowid, device_name from device_map;')
+    device_dict = {k:v for v, k in cur.fetchall()}
+
+    # remap process to an id
+    cur.execute('select rowid, process_path from process_map;')
+    process_dict = {k:v for v, k in cur.fetchall()}
+
+    col_replace = {
+                    'Center Name': 'center_name',\
+                    'Device Name': 'device_name',\
+                    'Process Name': 'process_name',\
+                    'Process Path': 'process_path',\
+                    'Operating System': 'os_id',\
+                    'OS Version': 'os_version_id',\
+                    'Is Server': 'is_server',\
+                    'Is Virtual':'is_virtual',\
+                    'Is Machine Internet Accessible': 'internet_accessible'
+                  }
+
+    LOG.info("Fixing data -- invoking remap")
+    data['center_id'] = data['Center Name'].replace(center_dict)
+    data['device_id'] = data['Device Name'].replace(device_dict)
+    data['process_id'] = data['Process Path'].replace(process_dict)
+
+    '''
+    data['os_id'] = data['Operating System'].replace(os_dict)
+    data['fips_199_cat_id'] = data['FIPS 199 Category'].replace(fips_dict)
+    data['impact_id'] = data['Impact'].replace(impact_dict)
+    data['vuln_scan_date_id'] = data['Vulnerability Scan Date'].replace(date_dict)
+    data['vuln_identifier_id'] = data['Vulnerability Identifier'].replace(vuln_dict)
+    data['os_version_id'] = data['OS Version'].replace(os_version_dict)
+    '''
+    # do the rename
+    data.rename(columns = col_replace, inplace=True)
+
+    LOG.info("data slicing")
+    device_data = data[["device_id", "center_id", "is_server",\
+#                        "is_virtual", "os_id", "os_version_id",\
+               ]].drop_duplicates() 
+    process_data = data[["process_id", "process_name", "process_path"]].drop_duplicates() 
+    print (process_data)
+
+    dev_proc_data = data[["process_id", "device_id"]].drop_duplicates() 
+
+    LOG.debug(device_data)
+    LOG.debug(dev_proc_data)
+
+    # build the tables now
+    from sqlalchemy import create_engine
+    engine = create_engine('postgresql://postgres@localhost:5432/'+dbname)
+
+    # update device metadata 
+    LOG.info("update device metadata")
+    device_data.to_sql('temp_table', engine, if_exists='replace', index=False)
+    sql = "UPDATE device_map AS f" + \
+          " SET center_id = t.center_id," +\
+          " is_server = t.is_server" + \
+          " FROM temp_table AS t" + \
+          " WHERE f.rowid = t.device_id"
+    cur.execute(sql)
+    conn.commit()
+
+    # update process metadata 
+    LOG.info("update process metadata")
+    process_data.to_sql('temp_table', engine, if_exists='replace', index=False)
+    sql = "UPDATE process_map AS f" + \
+          " SET process_name = t.process_name" +\
+          " FROM temp_table AS t" + \
+          " WHERE f.rowid = t.process_id"
+    cur.execute(sql)
+
+    # add new associations
+    LOG.info("add new dev/process associations")
+    dev_proc_data.to_sql('device_process_assoc', engine, if_exists='append', index=False)
+
+    # insert file into file_insert_log
+    cur.execute(f'''INSERT INTO file_insert_log (filename) VALUES ('{fname}');''')
+
+    conn.commit()
+    conn.close()
+
+def _parse(file: str) -> pd.DataFrame:
 
     df = None
     if file.find('gz') == -1:
@@ -34,21 +163,17 @@ if __name__ == '__main__':
     opts = ap.parse_args()
 
     if not opts.process:
-        print ("the --process <file> parameter must be specified")
+        LOG.fatal ("the --process <file> parameter must be specified")
         ap.print_usage()
         exit()
 
     if not opts.software:
-        print ("the --software <file> parameter must be specified")
+        LOG.fatal ("the --software <file> parameter must be specified")
         ap.print_usage()
         exit()
 
-    # parse out out data into in-memory DataFrames
-    process_data = parse(opts.process)
-    software_data = parse(opts.software)
-
     # load the data
+    _insert_process_files (opts.dbname, opts.process)
 
-    # print back to STDOUT
-
+    LOG.info ("finished")
 
