ce1087bb0dd7699bfb116a103e274762ef7b1c52
Brian Thomas <brian.a.thomas@nasa.gov>
2018-04-20 10:07:31 -0400

    fix db filters to allow compare of BFI with WR data

--- bin/db_util.py
+++ bin/db_util.py
@@ -2,6 +2,8 @@
 import pandas as pd
 from utility import *
 
+RDBS_DATA_TABLE = 'rdbs_data'
+
 # relational DB list from wikipedia
 # list of db software to search for
 
@@ -132,7 +134,7 @@ DB_SPECIFIC_CONSTRAINT_TERMS = {
    'Microsoft SQL Server%' : ['Analysis Service', 'Common Files', 'Engine Service', 'Integration Service', 'Notification Service', 'Reporting Service', 'Replication% Agent', 'Best Practices Analyzer', 'Management Studio', 'Master Data Service', 'Migration', 'PowerPivot', 'BI Development Studio', 'Registry Rebuild', 'Service Manager', 'VSS Writer', 'Management Objects', 'Language Service', 'Policies', 'Analysis Management', 'Dashboard', 'App Framework', 'Application Framework', 'Backward compatibility', 'OData', 'Publishing Wizard', 'ScriptDom', 'Addin for SharePoint', 'Add-in for SharePoint', 'Compiler Service', 'System CLR Types', 'Report Builder', 'Prerequisites', 'SQLXML4', 'ADOMD.NET', 'Data-Tier App', 'Design', 'Agent', 'Books', 'Remote BLOB Store', 'BPA' ], 
    'Microsoft Access%' : ['Control Panel', 'Database Packer'], 
    'SQL Anywhere%' : ['Studio', 'Sybase', 'SAP'], 
-   '%dBase%' : ['QuickReport', 'mold', 'Audbase' ], 
+   '%dBase %' : ['QuickReport', 'mold', 'Audbase' ], 
    '%Microsoft Visual FoxPro%' : ['Sedna'], 
    '%Panorama%' : ['32', 'Maker' ], 
    '%mSQL%' : ['Embarcadero TeamSQL', ], 
@@ -143,59 +145,58 @@ DB_SPECIFIC_CONSTRAINT_TERMS = {
    '%Aurora %' : ['Ground Processing', 'Blu-ray', 'HDR', 'Z88'],  
 } 
 
+def db_filter_constraint (db_software, webreport_constraints=True):
 
-def db_filter_constraint (db_software):
+    if webreport_constraints: 
+        db_no_percent_name = db_software.replace('%','')
+        db_constraint = f"""where (software_name ilike '{db_software}' or install_location ilike '%{db_no_percent_name}%')"""
+    else:
+        db_constraint = f"""where software_name ilike '{db_software}'"""
 
-    db_no_percent_name = db_software.replace('%','')
-    db_constraint = f"""where (software_name ilike '{db_software}' or install_location ilike '%{db_no_percent_name}%')"""
+    if not webreport_constraints: 
+        for term in DB_CONSTRAINT_TERMS_Caseless:
+            db_constraint += f""" and software_name not ilike '%{term}%'"""
 
-    '''
-    for term in DB_CONSTRAINT_TERMS_Caseless:
-        db_constraint += f""" and software_name not ilike '%{term}%'"""
-        # db_constraint += f""" and install_location not ilike '%{term}%'"""
-
-    for term in DB_CONSTRAINT_TERMS:
-        # db_constraint += f""" and install_location not like '%{term}%'"""
-        db_constraint += f""" and software_name not like '%{term}%'"""
-    '''
+        for term in DB_CONSTRAINT_TERMS:
+            db_constraint += f""" and software_name not like '%{term}%'"""
 
     if db_software in DB_SPECIFIC_CONSTRAINT_TERMS:
         for term in DB_SPECIFIC_CONSTRAINT_TERMS[db_software]:
             db_constraint += f""" and software_name not like '%{term}%'"""
-            # db_constraint += f""" and install_location not like '%{term}%'"""
+            #db_constraint += f""" and install_location not like '%{term}%'"""
 
     return db_constraint
 
 
 # Define a software search method or 2 for us
-def db_software (software, conn):
+def db_software (software, conn, table=RDBS_DATA_TABLE):
     ''' Simple program to automate software searches '''
     
-    return Query (conn, 'rdbs_data', db_filter_constraint(software)).result
+    return Query (conn, table, db_filter_constraint(software, webreport_constraints=table == RDBS_DATA_TABLE)).result
         
-def db_software_histo (software, conn):
+def db_software_histo (software, conn, table=RDBS_DATA_TABLE):
     '''Program to provide breakdown of types of software '''
 
-    return HistoQuery(conn, 'rdbs_data', 'software_name', db_filter_constraint(software)).result 
+    return HistoQuery(conn, table, 'software_name', db_filter_constraint(software, webreport_constraints=table == RDBS_DATA_TABLE)).result 
         
-def db_software_count (conn, software, table='rdbs_data', distinct=None):
+def db_software_count (conn, software, table=RDBS_DATA_TABLE, distinct=None):
     '''Determine count of matching software'''
     
-    return CountQuery (conn, table, db_filter_constraint(software), distinct).result
+    return CountQuery (conn, table, db_filter_constraint(software, webreport_constraints=table == RDBS_DATA_TABLE), distinct).result
 
-def count_rdbs_types(conn):
+def count_rdbs_types(conn, table=RDBS_DATA_TABLE):
     '''Count out all of the db software we can find'''
     
     data = []
     for db_software in RELATIONAL_DB_LIST:
-        data.append(pd.DataFrame.from_dict({'software_name': db_software.replace('%',''), 'num_types' : db_software_count(conn, db_software, distinct='software_name')['num']}))
+        data.append(pd.DataFrame.from_dict({'software_name': db_software.replace('%',''), 'num_types' : db_software_count(conn, db_software, table=table, distinct='software_name')['num']}))
     
     return pd.concat(data).sort_values(['num_types'], ascending=False)
     
-def count_rdbs_installs(conn):
+def count_rdbs_installs(conn, table='device_rdbs_basic'):
     
     data = []
     for db_software in RELATIONAL_DB_LIST:
-        data.append(pd.DataFrame.from_dict({'software_name': db_software.replace('%',''), 'num_installs' : db_software_count(conn, db_software, table='device_rdbs_basic')['num']}))
+        data.append(pd.DataFrame.from_dict({'software_name': db_software.replace('%',''), 'num_installs' : db_software_count(conn, db_software, table=table)['num']}))
         
     return pd.concat(data).sort_values(['num_installs'], ascending=False)
